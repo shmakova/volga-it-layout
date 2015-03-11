552d94439e44bbeba69fb349fb9d98dfc71b203e
shmakova <shmakova-nastya@yandex.ru>
2015-03-12 02:14:37 +0700

    доделки

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-/**
- * Created by shmakova on 05.03.15.
- */
+$("#youtube-video-btn").click(function() {
+	$("#youtube-video")
+		.removeClass("hidden")
+		.addClass("visible");
+});
+
+$("#registration-modal .reg-btn").click(function() {
+	$('#registration-modal').modal('hide')
+	$('#confirmation-modal').modal('show')
+});
+
